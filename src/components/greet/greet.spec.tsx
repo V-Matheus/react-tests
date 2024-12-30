e5e200b0e1a3936cb576b5e655ee1376ee4eac90
Victor Matheus <victormatheus507@gmail.com>
2024-12-30 14:08:31 -0300

    Filename Conventions

---
+++ src/components/greet/greet.spec.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react';
+import { Greet } from './greet';
+
+describe('Greet', () => {
+  it('renders correctly', () => {
+    render(<Greet />);
+    const textElement = screen.getByText('Hello');
+
+    expect(textElement).toBeInTheDocument();
+  });
+});
+
+describe('Nested', () => {
+  it('renders correctly', () => {
+    render(<Greet />);
+    const textElement = screen.getByText('Hello');
+
+    expect(textElement).toBeInTheDocument();
+  });
+});
